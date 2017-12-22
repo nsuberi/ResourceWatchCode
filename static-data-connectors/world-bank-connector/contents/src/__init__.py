25348400dc33bd0d5359093dcb33e314170f4aa1
nsuberi <nsuberi@gmail.com>
2017-12-22 11:19:40 -0500

    restructure existing code to templates

---
+++ static-data-connectors/world-bank-connector/contents/src/__init__.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
+
+from utilities import *
+
+def fetchData():
+    return(None)
+
+def parseData():
+    return(None)
+
+def uploadData():
+    return(None)
+
+def main():
+    fetchData()
+    parseData()
+    uploadData()
