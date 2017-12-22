6708fea21471a214d21d9c46955a2225f9b4b661
nsuberi <nsuberi@gmail.com>
2017-12-22 14:47:18 -0500

    add in useful_script for symlinking .env files

--- nrt-scripts/cli_025_mean_sea_level_rise/contents/src/__init__.py
+++ nrt-scripts/cli_025_mean_sea_level_rise/contents/src/__init__.py
@@ -6,36 +6,12 @@ logging.basicConfig(stream=sys.stderr, level=logging.ERROR)
 import pandas as pd
 import requests as req
 
-in_nrt_container = os.environ.get('IN_NRT_CONTAINER', False)
-
 # If not running in nrt container, environ variables will not be set
-if not in_nrt_container:
-    from configparser import ConfigParser
-    config = ConfigParser()
-    config.read("/Users/nathansuberi/Desktop/Code Portfolio/ResourceWatchCode/.env")
-    os.environ["CARTO_USER"] = "rw-nrt"
-    os.environ["CARTO_KEY"] = config.get("auth", "carto_api_token_nrt")
 
 from utilities import *
 
-CARTO_TABLE = 'test_floodreports'
+CARTO_TABLE = ''
 CARTO_SCHEMA = OrderedDict([
-    ('the_geom', 'geometry'),
-    ('_UID', 'text'),
-    ('ID', 'int'),
-    ('GlideNumber', 'text'),
-    ('Country', 'text'),
-    ('OtherCountry', 'text'),
-    ('long', 'numeric'),
-    ('lat', 'numeric'),
-    ('Area', 'numeric'),
-    ('Began', 'timestamp'),
-    ('Ended', 'timestamp'),
-    ('Validation', 'text'),
-    ('Dead', 'int'),
-    ('Displaced', 'int'),
-    ('MainCause', 'text'),
-    ('Severity', 'numeric')
 ])
 UID_FIELD = '_UID'
 TIME_FIELD = 'Began'
