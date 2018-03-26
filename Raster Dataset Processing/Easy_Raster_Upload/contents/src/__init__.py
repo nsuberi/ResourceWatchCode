5529af5f7e0eba15644841ea5224194d92f1d57b
nsuberi <nsuberi@gmail.com>
2018-03-26 10:40:18 -0400

    updating longform / georef tools

--- Raster Dataset Processing/Easy_Raster_Upload/contents/src/__init__.py
+++ Raster Dataset Processing/Easy_Raster_Upload/contents/src/__init__.py
@@ -4,36 +4,41 @@ import eeUtil
 import os
 from datetime import datetime
 
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-eeUtil.initJson()
+def main():
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    eeUtil.initJson()
 
-# Configure the ImageCollection you're going to add the rasters to
-GS_FOLDER = 'broad_age_groups'
-EE_COLLECTION = 'soc_075_broad_age_groups'
-def checkCreateCollection(collection):
-    '''List assests in collection else create new collection'''
-    if eeUtil.exists(collection):
-        return eeUtil.ls(collection)
-    else:
-        logging.info('{} does not exist, creating'.format(collection))
-        eeUtil.createFolder(collection, True, public=True)
-        return []
-existing_files = checkCreateCollection(EE_COLLECTION)
+    # Configure the ImageCollection you're going to add the rasters to
+    GS_FOLDER = 'wat_038_modis_surface_water'
+    EE_COLLECTION = 'wat_038_modis_surface_water'
+    def checkCreateCollection(collection):
+        '''List assests in collection else create new collection'''
+        if eeUtil.exists(collection):
+            return eeUtil.ls(collection)
+        else:
+            logging.info('{} does not exist, creating'.format(collection))
+            eeUtil.createFolder(collection, True, public=True)
+            return []
+    existing_files = checkCreateCollection(EE_COLLECTION)
 
-# Make sure your data is in the rasters folder
-DATA_DIR = 'rasters'
-EXTENSIONS = ['.tif', '.nc']
-os.chdir(DATA_DIR)
-tifs = [f for f in os.listdir('.') if os.path.splitext(f)[1] in EXTENSIONS]
-logging.info('TIFS: {}'.format(tifs))
+    # Make sure your data is in the rasters folder
+    DATA_DIR = 'rasters'
+    EXTENSIONS = ['.tif', '.nc']
+    os.chdir(DATA_DIR)
+    tifs = os.listdir('.') #[f for f in os.listdir('.') if os.path.splitext(f)[1] == '.tif']
+    logging.info('TIFS: {}'.format(tifs))
 
-# Update this manually, or with a function of the tif name
-DATE_FORMAT = '%Y'
-dates = ['2010', '2010', '2010', '2010', '2010', '2010']
-datestamps = [datetime.strptime(date, DATE_FORMAT)
-              for date in dates]
+    # Update this manually, or with a function of the tif name
+    DATE_FORMAT = '%Y%j'
 
-def ic(asset):
-    return '{}/{}'.format(EE_COLLECTION, os.path.splitext(asset)[0])
-asset_names = [ic(t) for t in tifs]
-eeUtil.uploadAssets(tifs, asset_names, GS_FOLDER, datestamps, public=True, timeout=30000)
+    def getDate(asset):
+        return asset[-7:]
+
+    dates = list(map(getDate, tifs))
+    datestamps = [datetime.strptime(date, DATE_FORMAT)
+                  for date in dates]
+
+    def ic(asset):
+        return '{}/{}'.format(EE_COLLECTION, os.path.splitext(asset)[0])
+    asset_names = [ic(t) for t in tifs]
+    eeUtil.uploadAssets(tifs, asset_names, GS_FOLDER, datestamps, public=True, timeout=30000)
