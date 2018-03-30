8d3fee4f7e05c73acbfa646a56109002e20d4032
nsuberi <nsuberi@gmail.com>
2018-03-29 22:28:19 -0400

    WIP uploading soilgrids from ftp to GEE and RW API

--- Raster Dataset Processing/Easy_Raster_Upload/contents/src/__init__.py
+++ Raster Dataset Processing/Easy_Raster_Upload/contents/src/__init__.py
@@ -5,12 +5,24 @@ import os
 from datetime import datetime
 
 def main():
-    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
-    eeUtil.initJson()
 
+    ###
+    # Configure logging
+    logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+    # Authenticate to GEE
+    eeUtil.initJson()
+    ###
+
+    ###
     # Configure the ImageCollection you're going to add the rasters to
+    ###
+
     GS_FOLDER = 'wat_038_modis_surface_water'
     EE_COLLECTION = 'wat_038_modis_surface_water'
+
+    def ic(asset):
+        return '{}/{}'.format(EE_COLLECTION, os.path.splitext(asset)[0])
+
     def checkCreateCollection(collection):
         '''List assests in collection else create new collection'''
         if eeUtil.exists(collection):
@@ -19,17 +31,69 @@ def main():
             logging.info('{} does not exist, creating'.format(collection))
             eeUtil.createFolder(collection, True, public=True)
             return []
+
     existing_files = checkCreateCollection(EE_COLLECTION)
 
-    # Make sure your data is in the rasters folder
-    DATA_DIR = 'rasters'
-    EXTENSIONS = ['.tif', '.nc']
-    os.chdir(DATA_DIR)
-    tifs = os.listdir('.') #[f for f in os.listdir('.') if os.path.splitext(f)[1] == '.tif']
-    logging.info('TIFS: {}'.format(tifs))
+    ###
+    # Obtain names of files to upload
+    # Load file names for tifs and netcdfs
+    ###
 
-    # Update this manually, or with a function of the tif name
-    DATE_FORMAT = '%Y%j'
+    # TIF_DATA_DIR = 'tifs'
+    # os.chdir(TIF_DATA_DIR)
+    # tifs = os.listdir('.') #[f for f in os.listdir('.') if os.path.splitext(f)[1] == '.tif']
+    # logging.info('TIFFs: {}'.format(tifs))
+    #
+    # NC_DATA_DIR = 'ncs'
+    # os.chdir(NC_DATA_DIR)
+    # ncs = os.listdir('.') #[f for f in os.listdir('.') if os.path.splitext(f)[1] == '.tif']
+    # logging.info('NetCDFs: {}'.format(ncs))
+
+    from ftplib import FTP
+    ftp = FTP('ftp.soilgrids.org')
+    ftp.login()
+
+    lines = []
+    ftp.retrlines('NLST', lines.append)
+
+    data = []
+    ftp.retrlines('NLST data/recent', data.append)
+
+    logging.info("Data:")
+    logging.info(data)
+
+    ###
+    # Retrieving legends for upload to RW API
+    ###
+
+    legends = []
+    ftp.retrlines('NLST legends', legends.append)
+    slds = [f for f in legends if os.path.splitext(f)[1] == '.sld']
+
+    for sld in slds:
+        ftp.retrbinary(sld)
+
+    # Q: Is this possible?
+    ### reduce(lambda obj, elem: obj.append(elem),  ftp.retrlines('NLST'), [])
+
+    ###
+    # Date formats vary by provider, some common ones include:
+    ###
+
+
+    DATE_FORMAT = '%Y%j' # Year and week of year
+    DATE_FORMAT = '%Y-%m-%d' # Year, month, day
+
+
+
+
+
+
+
+
+    ###
+    # To upload to GEE, need to specify the date
+    ###
 
     def getDate(asset):
         return asset[-7:]
@@ -38,7 +102,6 @@ def main():
     datestamps = [datetime.strptime(date, DATE_FORMAT)
                   for date in dates]
 
-    def ic(asset):
-        return '{}/{}'.format(EE_COLLECTION, os.path.splitext(asset)[0])
+
     asset_names = [ic(t) for t in tifs]
     eeUtil.uploadAssets(tifs, asset_names, GS_FOLDER, datestamps, public=True, timeout=30000)
