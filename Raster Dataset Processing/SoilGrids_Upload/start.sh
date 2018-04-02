f4b527e2c5db291b06851e7db0448cd52a844372
nsuberi <nsuberi@gmail.com>
2018-04-02 13:46:09 -0400

    update start script to have volumes

--- Raster Dataset Processing/SoilGrids_Upload/start.sh
+++ Raster Dataset Processing/SoilGrids_Upload/start.sh
@@ -5,7 +5,8 @@ LOG=${LOG:-udp://localhost}
 docker build -t $NAME --build-arg NAME=$NAME .
 docker run \
     --log-driver=syslog --log-opt syslog-address=$LOG --log-opt tag=$NAME \
-    --volume="$(pwd)"/rasters:/opt/$NAME/rasters \
+    --volume="$(pwd)"/tifs:/opt/$NAME/tifs \
+    --volume="$(pwd)"/ncs:/opt/$NAME/ncs \
     --env-file .env \
     --rm $NAME \
     python main.py
